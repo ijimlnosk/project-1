49eeb258eabe57af110052b2e509e8d8b2190acc
ijimnlosk <skek298@naver.com>
2024-12-24 18:08:37 +0900

    💡refactor: 반응형 글씨 크기 수정

--- src/components/twenty-four.tsx/washroom.tsx
+++ src/components/twenty-four.tsx/washroom.tsx
@@ -27,13 +27,13 @@ const WashRoom = () => {
             <PositionedElement
                 text="안방 화장실"
                 onClick={() => setIsWashRoom1Open(true)}
-                className="position-min-24-right-washroom-location h-[40%] w-10 text-[6px]"
+                className="position-min-24-right-washroom-location h-[40%] w-10 text-[8px]"
                 responsiveClassName="sm:top-32 sm:right-8 sm:w-[14.5%] sm:text-xs md:top-48 md:right-[49px] md:h-[30%]"
             />
             <PositionedElement
                 text="거실 화장실"
                 onClick={() => setIsWashRoom2Open(true)}
-                className="position-min-24-left-washroom-location h-[40%] w-10 text-[6px]"
+                className="position-min-24-left-washroom-location h-[40%] w-10 text-[8px]"
                 responsiveClassName="sm:top-[86px] sm:left-24 sm:w-[14.5%] sm:text-xs md:top-32 md:left-36 md:h-[30%]"
             />
         </div>
